8765836b9dd4079e5e92a1c2f44bd1ab21e8f211
Tauane <tauane-soares@hotmail.com>
2025-09-02 14:50:40 -0300

    classe abs e interface

--- Menu.ts
+++ Menu.ts
@@ -8,14 +8,6 @@ export function main(){
     let opcao: number;
 
     
-    const conta: Conta = new Conta(1, 123, 1, "Adriana", 10000);
-    conta.visualizar();
-    conta.sacar(10500);
-    conta.visualizar();
-    conta.depositar(5000);
-    conta.visualizar();
-
-   
     const contacorrente: ContaCorrente = new ContaCorrente(2, 123, 1, "Mariana", 15000, 1000);
     contacorrente.visualizar();
     contacorrente.sacar(2000);
