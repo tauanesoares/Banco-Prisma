7d7a36f69b5f2bb705b2a1d294c152a6acfcaacc
Tauane <tauane-soares@hotmail.com>
2025-09-04 12:47:07 -0300

    parte 1 controller

--- Menu.ts
+++ Menu.ts
@@ -3,25 +3,33 @@ import { colors } from "./src/util/Colors";
 import { Conta } from "./src/model/Conta";
 import { ContaCorrente } from "./src/model/ContaCorrente";
 import { ContaPoupanca } from "./src/model/ContaPoupanca";
+import { ContaController } from "./src/controller/ContaController";
 
 export function main(){
-    let opcao: number;
+    //instancia da classe ContaController
+    let contas: ContaController = new ContaController();
 
-    
-    const contacorrente: ContaCorrente = new ContaCorrente(2, 123, 1, "Mariana", 15000, 1000);
-    contacorrente.visualizar();
-    contacorrente.sacar(2000);
-    contacorrente.visualizar();
-    contacorrente.depositar(1000);
-    contacorrente.visualizar();
+    //variaveis auxiliares
+    let opcao, numero, agencia, tipo, saldo, limite, aniversario: number;
+    let titular: string;
 
-    
-    const contapoupanca: ContaPoupanca = new ContaPoupanca(3, 123, 2, "Victor", 1000, 10);
-    contapoupanca.visualizar();
-    contapoupanca.sacar(200);
-    contapoupanca.visualizar();
-    contapoupanca.depositar(1000);
-    contapoupanca.visualizar();
+    const tiposContas = ['Conta Corrente', 'Conta Poupanca'];
+
+    console.log("\nCriar Contas\n");
+
+    let cc1: ContaCorrente = new ContaCorrente(contas.gerarNumero(), 123, 1, "João da Silva", 1000, 100.0);
+    contas.cadastrar(cc1);
+
+    let cc2: ContaCorrente = new ContaCorrente(contas.gerarNumero(), 124, 1, "Maria da Silva", 2000, 100.0);
+    contas.cadastrar(cc2);
+
+    let cp1: ContaPoupanca = new ContaPoupanca(contas.gerarNumero(), 125, 2, "Mariana dos Santos", 4000, 12);
+    contas.cadastrar(cp1);
+
+    let cp2: ContaPoupanca = new ContaPoupanca(contas.gerarNumero(), 125, 2, "Juliana Ramos", 8000, 15);
+    contas.cadastrar(cp2);
+
+    contas.listarTodas();
 
     while(true){
         console.log(colors.bg.black, colors.fg.magentastrong, 
@@ -60,13 +68,37 @@ export function main(){
             case 1:
                 console.log(colors.fg.whitestrong, 
                     "\n\nCriar Conta\n\n", colors.reset);
-                
+                console.log("Digite o numero da Agência: ");
+                agencia = readlinesync.questionInt("");
+
+                console.log("Digite o nome do Titular da Conta: ");
+                titular = readlinesync.question("");
+
+                console.log("Escolha o Tipo da Conta: ");
+                tipo = readlinesync.keyInSelect(tiposContas, "", {cancel: false}) + 1;
+
+                console.log("Digite o saldo da conta: ");
+                saldo = readlinesync.questionFloat("");
+
+
+                switch (tipo) {
+                    case 1:
+                        console.log("Digite o limite da conta: ");
+                        limite = readlinesync.questionFloat("");
+                        contas.cadastrar(new ContaCorrente(contas.gerarNumero(), agencia, tipo, titular, saldo, limite));
+                        break;
+                    case 2:
+                        console.log("Digite o dia do aniversário da conta poupança: ");
+                        aniversario = readlinesync.questionInt("");
+                        contas.cadastrar(new ContaPoupanca(contas.gerarNumero(), agencia, tipo, titular, saldo, aniversario));
+                        break;
+                }
                 keyPress()
                 break;
             case 2:
                 console.log(colors.fg.whitestrong, 
                     "\n\nListar todas as Contas\n\n", colors.reset);
-
+                contas.listarTodas();
                 keyPress()
                 break;
             case 3:
---
+++ src/controller/ContaController.ts
@@ -0,0 +1,44 @@
+import { Conta } from "../model/Conta";
+import { ContaRepository } from "../repository/ContaRepository";
+import { colors } from "../util/Colors";
+
+export class ContaController implements ContaRepository {
+
+    private listaContas: Array<Conta> = new Array<Conta>();
+    numero: number = 0;
+
+    listarTodas(): void{
+        for (let conta of this.listaContas){
+            conta.visualizar();
+        };
+    }
+    procurarPorNumero(numero: number): void {
+        throw new Error("Method not implemented.");
+    }
+    cadastrar(conta: Conta): void {
+        this.listaContas.push(conta);
+        console.log(colors.fg.green, "\nA conta número: " + conta.numero + " foi criada com sucesso!\n", colors.reset);
+    }
+    atualizar(conta: Conta): void {
+        throw new Error("Method not implemented.");
+    }
+    deletar(numero: number): void {
+        throw new Error("Method not implemented.");
+    }
+    sacar(numero: number, valor: number): void {
+        throw new Error("Method not implemented.");
+    }
+    depositar(numero: number, valor: number): void {
+        throw new Error("Method not implemented.");
+    }
+    transferir(numeroOrigem: number, numeroDestino: number, valor: number): void {
+        throw new Error("Method not implemented.");
+    }
+
+    //gerar número de conta
+    public gerarNumero(): number {
+        return ++this.numero;
+    }
+
+    
+}
